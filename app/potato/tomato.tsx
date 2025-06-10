d4478f3072ab4ba41e339857f8151fc52a71a56d
SeungLee <ls0408@naver.com>
2025-06-10 21:05:08 +0900

    # 1.1 Routes File

---
+++ app/potato/tomato.tsx
@@ -0,0 +1,4 @@
+// 반드시 export default 해야 한다. 아니면 에러 발생
+export default function AboutUs() {
+	return <div>Tomato</div>;
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+/**
+ * 사용자 url의 위치에 따라 렌더링할 내용을 정의해서 react router에 알려주는 파일
+ * react router는 하나의 프레임 워크
+ * routes.ts 파일은 선택 사항이 아니고 이름도 고정이다. /app 폴더 안에 있어야 한다.
+ *
+ * index function
+ * 사용자가 웹사이트의 홈으로 가는 index  페이지로 이동했을 때, home.tsx 컴포넌트를 렌더링할 것을
+ * react 라우터에 알려주는 함수
+ * route의 array를 export default 해야한다.
+ */
+export default [
+	index('routes/home.tsx'),
+	route('/about', 'potato/tomato.tsx'),
+] satisfies RouteConfig;
