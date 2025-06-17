336d4d7750c9446d4759e6a7238184a7c9d8f579
SeungLee <ls0408@naver.com>
2025-06-17 21:30:48 +0900

    #3.4 Navigation part Three

--- app/common/components/navigation.tsx
+++ app/common/components/navigation.tsx
@@ -10,6 +10,25 @@ import {
 	navigationMenuTriggerStyle,
 } from './ui/navigation-menu';
 import { cn } from '~/lib/utils';
+import { Button } from './ui/button';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import {
+	BarChart3Icon,
+	BellIcon,
+	LogOutIcon,
+	MessageCircleIcon,
+	SettingsIcon,
+	UserIcon,
+} from 'lucide-react';
 
 const menues = [
 	{
@@ -121,7 +140,15 @@ const menues = [
 		],
 	},
 ];
-export default function Navigation() {
+export default function Navigation({
+	isLoggedIn = false,
+	hasNotifications = false,
+	hasMessages = false,
+}: {
+	isLoggedIn: boolean;
+	hasNotifications: boolean;
+	hasMessages: boolean;
+}) {
 	return (
 		<nav className='flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50'>
 			<div className='flex items-center'>
@@ -144,15 +171,14 @@ export default function Navigation() {
 													<NavigationMenuItem
 														className={cn(
 															'select-none rounded-md transition-colors focus:bg-accent hover:bg-accent',
-															item.to === '/products/promote' &&
-																'col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20',
-															item.to === '/jobs/submit' &&
+															(item.to === '/products/promote' ||
+																item.to === '/jobs/submit') &&
 																'col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20'
 														)}
 														key={item.name}
 													>
 														<NavigationMenuLink asChild>
-															{/* asChild를 붙여주면 nagicationMenuLink의 스타일이 자식 Link에 적용된다. 해당 컴포넌트가 아닌 react router의 link를 사용하기 위해 사용*/}
+															{/* asChild를 붙여주면 nagicationMenuLink의 스타일이 or prop이 자식 Link에 적용된다. 해당 컴포넌트가 아닌 react router의 link를 사용하기 위해 사용*/}
 															<Link
 																className='p-3 space-y-1 block leading-none no-underline outline-none'
 																to={item.to}
@@ -180,6 +206,77 @@ export default function Navigation() {
 					</NavigationMenuList>
 				</NavigationMenu>
 			</div>
+			{isLoggedIn ? (
+				<div className='flex items-center gap-2'>
+					<Button size='icon' variant='ghost' asChild className='relative'>
+						<Link to='/my/notifications'>
+							<BellIcon className='size-4' />
+							{hasNotifications && (
+								<div className='absolute top-1.5 right-1.5 size-2 bg-red-500 rounded-full' />
+							)}
+						</Link>
+					</Button>
+					<Button size='icon' variant='ghost' asChild className='relative'>
+						<Link to='/my/messages'>
+							<MessageCircleIcon className='size-4' />
+							{hasMessages && (
+								<div className='absolute top-1.5 right-1.5 size-2 bg-red-500 rounded-full' />
+							)}
+						</Link>
+					</Button>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Avatar>
+								<AvatarImage src='https://github.com/eiru123.png' />
+								<AvatarFallback>N</AvatarFallback>
+							</Avatar>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent className='w-56'>
+							<DropdownMenuLabel className='flex flex-col'>
+								<span className='font-medium'>John Doe</span>
+								<span className='text-xs text-muted-foreground'>@username</span>
+							</DropdownMenuLabel>
+							<DropdownMenuSeparator />
+							<DropdownMenuGroup>
+								<DropdownMenuItem asChild className='cursor-pointer'>
+									<Link to='/my/dashboard'>
+										<BarChart3Icon className='size-4 mr-2' />
+										Dashboard
+									</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem asChild className='cursor-pointer'>
+									<Link to='/my/profile'>
+										<UserIcon className='size-4 mr-2' />
+										Profile
+									</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem asChild className='cursor-pointer'>
+									<Link to='/my/settings'>
+										<SettingsIcon className='size-4 mr-2' />
+										Settings
+									</Link>
+								</DropdownMenuItem>
+							</DropdownMenuGroup>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem asChild className='cursor-pointer'>
+								<Link to='/auth/logout'>
+									<LogOutIcon className='size-4 mr-2' />
+									Logout
+								</Link>
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+			) : (
+				<div className='flex items-center gap-4'>
+					<Button asChild variant='secondary'>
+						<Link to='/auth/login'>Login</Link>
+					</Button>
+					<Button asChild>
+						<Link to='/auth/join'>Join</Link>
+					</Button>
+				</div>
+			)}
 		</nav>
 	);
 }
--- app/root.tsx
+++ app/root.tsx
@@ -81,7 +81,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
 	return (
 		<>
-			<Navigation />
+			<Navigation
+				isLoggedIn={true}
+				hasNotifications={true}
+				hasMessages={true}
+			/>
 			<Outlet />
 		</>
 	);
