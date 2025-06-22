8a7b441d019b7ddb5dd8277db34653d82bbc5114
SeungLee <ls0408@naver.com>
2025-06-22 14:05:21 +0900

    #3.9 Team Card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -3,7 +3,11 @@ import ProductCard from '~/features/products/components/product-card';
 import PostCard from '~/features/community/components/post-card';
 import IdeaCard from '~/features/ideas/components/idea-card';
 import JobCard from '~/features/jobs/components/job-card';
+import TeamCard from '~/features/teams/components/team-card';
 import { Button } from '../components/ui/button';
+import { Card, CardFooter, CardHeader, CardTitle } from '../components/ui/card';
+import { Badge } from '../components/ui/badge';
+import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar';
 
 export const meta: MetaFunction = () => {
 	return [
@@ -108,6 +112,32 @@ export default function HomePage() {
 						positionLocation='Remote'
 						salary='$100,000 - $120,000'
 						companyHq='San Francisco, CA'
+					/>
+				))}
+			</div>
+			<div className='grid grid-cols-4 gap-4'>
+				<div>
+					<h2 className='text-5xl font-bold leading-tight tracking-tight'>
+						Find a team mate
+					</h2>
+					<p className='text-xl font-light text-foreground'>
+						Join a team looking for a new member.
+					</p>
+					<Button variant='link' asChild className='text-lg p-0'>
+						<Link to='/teams'>Explore all teams &rarr;</Link>
+					</Button>
+				</div>
+				{Array.from({ length: 7 }).map((_, index) => (
+					<TeamCard
+						id={`teamId-${index}`}
+						leaderUserName='lynn'
+						leaderAvatarUrl='https://github.com/inthetiger.png'
+						positions={[
+							'React Developer',
+							'Backend Developer',
+							'Product Manager',
+						]}
+						projectDescription='a new social media platform'
 					/>
 				))}
 			</div>
