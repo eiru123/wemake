df1607104c3561fd5b5b082453028925e80068cd
SeungLee <ls0408@naver.com>
2025-07-31 19:45:04 +0900

    #5.8 Suspense and Await

--- app/features/community/pages/community-page.tsx
+++ app/features/community/pages/community-page.tsx
@@ -4,7 +4,7 @@ import {
 	DropdownMenuTrigger,
 } from '~/common/components/ui/dropdown-menu';
 import { ChevronDownIcon } from 'lucide-react';
-import { Link, useSearchParams, Form } from 'react-router';
+import { Link, useSearchParams, Form, Await } from 'react-router';
 import { Hero } from '~/common/components/hero';
 import { Button } from '~/common/components/ui/button';
 import { DropdownMenu } from '~/common/components/ui/dropdown-menu';
@@ -13,6 +13,7 @@ import { Input } from '~/common/components/ui/input';
 import PostCard from '../components/post-card';
 import { getPosts, getTopics } from '../queries';
 import type { Route } from './+types/community-page';
+import { Suspense } from 'react';
 
 export function meta() {
 	return [{ title: 'Community | Wemake' }];
@@ -21,13 +22,19 @@ export function meta() {
 // backend에서 실행
 // 로딩이 오래걸리면 사용자는 화면을 보지 못함.
 export const loader = async () => {
-	await new Promise((resolve) => setTimeout(resolve, 10000));
+	// await new Promise((resolve) => setTimeout(resolve, 10000));
+	// const topics = await getTopics();
+	// const posts = await getPosts();
+	// const [topics, posts] = await Promise.all([getTopics(), getPosts()]);
+	// await 없이 내보낸다.
+	// frontend 코드에서 Await 컴포넌트에 전달
 	const topics = await getTopics();
-	const posts = await getPosts();
+	const posts = getPosts();
 	return { topics, posts };
 };
 
 export default function CommunityPage({ loaderData }: Route.ComponentProps) {
+	const { topics, posts } = loaderData;
 	const [searchParams, setSearchParams] = useSearchParams();
 	const sorting = searchParams.get('sorting') || 'newest';
 	const period = searchParams.get('period') || 'all';
@@ -101,29 +108,36 @@ export default function CommunityPage({ loaderData }: Route.ComponentProps) {
 							<Link to='/community/submit'>CreateDiscussion</Link>
 						</Button>
 					</div>
-					<div className='space-y-5'>
-						{loaderData.posts.map((post) => (
-							<PostCard
-								key={post.post_id}
-								id={post.post_id}
-								title={post.title}
-								author={post.author}
-								authorAvatarUrl={post.author_avatar}
-								category={post.topic}
-								postedAt={post.created_at}
-								avatarFallback='A'
-								votesCount={post.upvotes}
-								expanded
-							/>
-						))}
-					</div>
+					<Suspense fallback={<div>Loading...</div>}>
+						<Await resolve={posts}>
+							{(data) => (
+								<div className='space-y-5'>
+									{data.map((post) => (
+										<PostCard
+											key={post.post_id}
+											id={post.post_id}
+											title={post.title}
+											author={post.author}
+											authorAvatarUrl={post.author_avatar}
+											category={post.topic}
+											postedAt={post.created_at}
+											avatarFallback='A'
+											votesCount={post.upvotes}
+											expanded
+										/>
+									))}
+								</div>
+							)}
+						</Await>
+					</Suspense>
 				</div>
 				<aside className='col-span-2 space-y-4'>
 					<span className='block text-sm font-bold text-muted-foreground'>
 						Topics
 					</span>
+
 					<div className='flex flex-col gap-4 items-start'>
-						{loaderData.topics.map((topic) => (
+						{topics.map((topic) => (
 							<Button variant='link' asChild className='pl-0'>
 								<Link
 									key={topic.slug}
